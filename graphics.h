433a0a6b7d3c9552f92b0d58be066c3cec1cd54c
connor eyers <connor.eyers@icloud.com>
2022-04-25 23:34:06 +0100

    Added game of life

--- graphics.h
+++ graphics.h
@@ -2,7 +2,7 @@ void initialise_fb();
 void pixel(int x, int y, unsigned int col);
 void drawChar(unsigned char c, int x, int y, unsigned int col, unsigned int bgcol);
 void drawString(unsigned char *s, int x, int y, unsigned int col, unsigned int bgcol);
-void clear(unsigned int col);
+void clearScreen(unsigned int col);
 void g_putc(void* p, char c);
 extern unsigned int width;
 extern unsigned int height;
--- kernel.c
+++ kernel.c
@@ -7,6 +7,7 @@
 #include "mem.h"
 #include "timer.h"
 #include "irq_handler.h"
+#include "gameoflife.h"
 
 void delay(unsigned int count)
 {
@@ -38,7 +39,8 @@ void kernel_main(unsigned int r0, unsigned int r1, unsigned int atags)
 	
 	drawString((unsigned char *)0x0, 0, 409, 0xFF00, 0xFF00FF);
 	
- 
+ 	gameoflife();
+ 	
 	while (1)
 		uart_putc(uart_getc());
 }
